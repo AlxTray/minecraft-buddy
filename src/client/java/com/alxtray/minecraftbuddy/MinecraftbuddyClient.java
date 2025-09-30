56928bd30c73607bf04dba1d1ecc1f8356b1a4e0
AlxTray <alex.ashby02@icloud.com>
2025-09-30 19:19:34 +0100

    Added template mod starter

---
+++ src/client/java/com/alxtray/minecraftbuddy/MinecraftbuddyClient.java
@@ -0,0 +1,10 @@
+package com.alxtray.minecraftbuddy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinecraftbuddyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
